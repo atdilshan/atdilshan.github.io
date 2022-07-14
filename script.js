4e82e5f818acea35e0d5d59098da39720bd08954
Dilshan Antony Thevathas <atdilshan0127@gmail.com>
2022-07-14 12:39:56 +0530

    Delete script.js file

--- script.js
+++
@@ -1,7 +0,0 @@
-const menuBtn = document.getElementById("menu-btn");
-const menu = document.getElementById("menu");
-
-menuBtn.onclick = function(){
-    menu.classList.toggle("active");
-    menuBtn.classList.toggle("active");
-}
