c76a13c827ea107d33bb77819d88b3eb92183a07
ATDilshan <atdilshan0127@gmail.com>
2022-07-14 12:36:56 +0530

    New Style

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// // typing text animation script
+// var typed = new Typed(".typing", {
+//     strings: ["Developer", "Freelancer", "Designer", "YouTuber", "Blogger", "IT Enthusiast"],
+//     typeSpeed: 100,
+//     backSpeed: 60,
+//     loop: true
+// });
